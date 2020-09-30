ff6cfbfcfe1c9e1ae972da6df9287c540ad6b27e
umeshramya <umeshbilagi@gmail.com>
2020-09-30 09:01:56 +0530

    added icon

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to TheMedPG.com!</Text>
+      <Text>Welcome to TheMedPG!</Text>
       <StatusBar style="auto" />
     </View>
   );
