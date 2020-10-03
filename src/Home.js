62dc45146e54affc6bad163ab758b6a200cf0cd2
umeshramya <umeshbilagi@gmail.com>
2020-10-03 19:56:43 +0530

    issue reod debug

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 
 export default function Home(props) {
   return (
     <View style={styles.style}>
       <Text>Home</Text>
+      <Button>Login</Button>
     </View>
   );
 }
